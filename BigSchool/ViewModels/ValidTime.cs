723c7cccc94a6de19f17b6526c031ce798823412
manhy <manhy@DESKTOP-FR1KSIR>
2021-07-05 16:31:28 +0700

    Add project files.

---
+++ BigSchool/ViewModels/ValidTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace BigSchool.ViewModels
+{
+    internal class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                                                "dd/M/yyyy",
+                                                CultureInfo.CurrentCulture,
+                                                DateTimeStyles.None,
+                                                out dateTime);
+            return (isValid && dateTime > DateTime.Now);
+        }
+    }
+}
